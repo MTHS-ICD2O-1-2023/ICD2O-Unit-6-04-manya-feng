c8c7a80694c4697715c01f1fd6529a8baeccf38b
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-24 18:34:29 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("radius")
+  console.log(radius)
+
+  // process
+  const volume = 4.0 / 3.0 * Math.PI * Math.pow(radius, 3)
+  const dimensions =
+    "<ul>\n<li>radius = " +
+    "</li>\n</ul>"
+
+  // output
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("volume").innerHTML = "Volume is: " + volume + " mm³"
+}
